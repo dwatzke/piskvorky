8c06da80e3e2c6ed57b8bed4cecc3791dbe2e4eb
David Watzke <david@watzke.cz>
2008-11-03 18:23:12 +0100

    drop destructors, assign proper parents to all classes instead, add Czech translation, fix a bug (gameover check doesn't work on 5x5 board)

---
+++ piskvorky_cs_CZ.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="cs">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="board.cpp" line="95"/>
+        <source>Status:</source>
+        <translation>Stav:</translation>
+    </message>
+    <message>
+        <location filename="board.cpp" line="97"/>
+        <source>Red</source>
+        <translation>Červený</translation>
+    </message>
+    <message>
+        <location filename="board.cpp" line="99"/>
+        <source>Blue</source>
+        <translation>Modrý</translation>
+    </message>
+    <message>
+        <location filename="board.cpp" line="103"/>
+        <source>&apos;s turn...</source>
+        <translation> je na řadě...</translation>
+    </message>
+    <message>
+        <location filename="board.cpp" line="207"/>
+        <source> wins.</source>
+        <translation> vítězí.</translation>
+    </message>
+</context>
+</TS>
